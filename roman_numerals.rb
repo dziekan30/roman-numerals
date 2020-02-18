049b2c7fda29433e0a03e5f666fcb698caabf7f2
“dziekan30” <dziekan30@gmail.com>
2020-02-18 17:02:18 -0600

    new

---
+++ roman_numerals.rb
@@ -0,0 +1,46 @@
+=begin
+Write your code for the 'Roman Numerals' exercise in this file. Make the tests in
+`roman_numerals_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/roman-numerals` directory.
+=end
+
+
+class Integer
+
+  def numbers 
+         {
+        1000 => "M",  
+        900 => "CM",  
+        500 => "D",  
+        400 => "CD",
+        100 => "C",  
+        90 => "XC",  
+        50 => "L",  
+        40 => "XL",  
+        10 => "X",  
+        9 => "IX",  
+        5 => "V",  
+        4 => "IV",  
+        1 => "I"  
+
+        }
+  end
+
+
+  def to_roman
+    n = self
+    r = ""
+  numbers.each do |k, v|
+    r << v*(n/k)
+    n = n % k
+    end
+    r
+  end
+
+end
+
+
+  
+
